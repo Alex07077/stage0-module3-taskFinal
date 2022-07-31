38140b9b4c31552149d086323faafe69235c15fb
Alex Myachin <myachin_aleksei@mail.ru>
2022-08-01 00:16:41 +0600

    Done final module task.

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -1,6 +1,13 @@
 package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
-    public void revert(int number) {
+        public void revert(int number) {
+           int num = 0;
+            for (int i = 0; i < 3; i++){
+                num += (number % 10);
+                number/=10;
+                num *= 10;
+        }
+            System.out.println(num/10);
     }
 }
